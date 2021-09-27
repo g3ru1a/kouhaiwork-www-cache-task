6e689b102223c21d862cc62c4ff0bb409d5b2545
G3ru1a <egidiufarcas@maze.ws>
2021-09-27 17:45:45 +0100

    added cron job

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
 const express = require("express");
-let { createClient } = require("redis");
-var cors = require("cors");
-let bodyParser = require("body-parser");
+const { createClient } = require("redis");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cron = require("node-cron");
 
 const app = express();
+let seriesToClear = [];
 // Set  our default port
 app.set("port", process.env.PORT || 3456);
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
-app.post('/flush/', async function(req, res){
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post("/flush/", async function (req, res) {
     console.log(req.body);
     let series = JSON.parse(req.body.series);
-    if(series==null) res.sendStatus(422);
-    let redisClient  = await connectRedis();
-    
+    if (series == null) res.sendStatus(422);
+
     for (let i = 0; i < series.length; i++) {
         let s = series[i];
-        await redisClient.del('series-'+s);
+       seriesToClear.push(s);
     }
-
-    await redisClient.del('homepage-latest');
-    await redisClient.del('homepage-week');
-    await redisClient.del("homepage-all");
-
     res.sendStatus(200);
-})
+});
 
-app.get('/', function(req, res){
-    res.send('Cache-kun gommene!');
+app.get("/", function (req, res) {
+    res.send("Cache-kun gommene!");
+});
+
+cron.schedule("*/5 * * * *", async () => {
+    if(seriesToClear.length > 0){
+        let redisClient = await connectRedis();
+
+        for (let i = 0; i < seriesToClear.length; i++) {
+            let s = seriesToClear[i];
+            await redisClient.del("series-" + s);
+        }
+        seriesToClear = [];
+
+        await redisClient.del("homepage-latest");
+        await redisClient.del("homepage-week");
+        await redisClient.del("homepage-all");
+    }
 });
 
 // Initialize our server
-app.listen(app.get("port"), err => {
-  // If something goes wrong when initializing the server
-  if (err) console.log(`Server failure due to ${err.message}`);
-  console.log(`Server running on port ${app.get("port")}`);
+app.listen(app.get("port"), (err) => {
+    // If something goes wrong when initializing the server
+    if (err) console.log(`Server failure due to ${err.message}`);
+    console.log(`Server running on port ${app.get("port")}`);
 });
 
-async function connectRedis(){
+async function connectRedis() {
     let client = createClient();
     client.on("ready", () => {
         console.log("Connected to redis");
